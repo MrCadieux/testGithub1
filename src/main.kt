944c3a5668e13f2966ae4ed06d8546ecf9028d06
Jeff Cadieux <jcadieux@d155.org>
2020-02-17 12:51:45 -0600

    Initial Commit Failed - ReUpload

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Jeff Cadieux")
+    println("My parents owned a bakery while I grew up")
+    println("Math teacher at CLS")
+    println("No pets. I used to have a cat and can't bring myself to have another")
+    println("Basic, Pascal, C++, Java, JavaScript...all at a low level")
+
+}
